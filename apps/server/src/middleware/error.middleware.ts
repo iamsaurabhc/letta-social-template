ddaf0940d8823f526c9560d6b004ea0e2bfcdd1b
saurabhc <iamsaurabhc@gmail.com>
2025-02-18 20:16:08 +0530

    middleware update

--- apps/server/src/middleware/error.middleware.ts
+++ apps/server/src/middleware/error.middleware.ts
@@ -1,11 +1,10 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
+    const response = ctx.getResponse();
     
     const status = 
       exception instanceof HttpException
