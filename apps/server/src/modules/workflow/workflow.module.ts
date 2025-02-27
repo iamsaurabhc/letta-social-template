24db8d1f214dec67d92e465f36ec9d9b4911b253
saurabhc <iamsaurabhc@gmail.com>
2025-02-27 16:09:32 +0530

    updated imports

--- apps/server/src/modules/workflow/workflow.module.ts
+++ apps/server/src/modules/workflow/workflow.module.ts
@@ -1,9 +1,16 @@
 import { Module } from "@nestjs/common";
 import { WorkflowController } from "./workflow.controller";
 import { WorkflowService } from "./workflow.service";
+import { LettaModule } from "../letta/letta.module";
+import { SocialModule } from "../social/social.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    LettaModule,
+    SocialModule,
+    ConfigModule,
+  ],
   controllers: [WorkflowController],
   providers: [WorkflowService],
   exports: [WorkflowService]
